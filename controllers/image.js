63e05a4f83b5ee33e1dc141729eb24b4aaacbe23
overlord-17 <deogharia.17@gmail.com>
2021-03-04 12:22:25 +0530

    fixing some errors

--- controllers/image.js
+++ controllers/image.js
@@ -1,5 +1,5 @@
 const Clarifai = require('clarifai');
- 
+
 const app = new Clarifai.App({
  apiKey: 'b61dd025b4884682af19ad7ae9471954'
 });
@@ -22,18 +22,23 @@ const handleApiCall = (req, res) => {
     .catch(err => res.status(400).json('unable to work with API'))
 }
 
-// const handleImage = (req, res, db) => {
-//   const { id } = req.body;
-//   db('users').where('id', '=', id)
-//   .increment('entries', 1)
-//   .returning('entries')
-//   .then(entries => {
-//     res.json(entries[0]);
-//   })
-//   .catch(err => res.status(400).json('unable to get entries'))
-// }
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
 
 module.exports = {
-  // handleImage,
+  handleImage,
   handleApiCall
 }
+
+
+
+
+
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
-// const profile = require('./controllers/profile');
+const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
@@ -26,7 +26,7 @@ app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-// app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
